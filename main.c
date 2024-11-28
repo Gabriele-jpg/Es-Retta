a5664e34ed286d9979ef1ec07a54f97c0d5d13d7
Gabriele-jpg <gabriele.dioguardi07@gmail.com>
2024-11-28 17:53:12 +0100

    Esercizio sulla Retta (1 warning)

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int Retta (float x, float m, float y, float q) {
+    return y == (x + m + q);
+}
+
+int main(void) {
+    float x, m, y, q;
+    printf("Inserisci il valore m: \n");
+    scanf("%f", &m);
+    printf("Inserisci il valore q: \n");
+    scanf("%f", &q);
+
+
+    printf("inserisci le coordinate dei punti (x e y):");
+    scanf("%f,%f", &x,&y);
+
+    if (Retta(x, y, m, q)) {
+        printf("Il punto (%.2f, %.2f) appartiene alla retta y = %.2fx + %.2f\n", x, y, m, q);
+    } else
+    { printf("Il punto (%.2f, %.2f) non appartiene alla retta y = %.2fx + %.2f\n", x, y, m, q);
+
+
+
+
+
+        return 0;
+    }
+}
